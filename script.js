f2bc3e71c97f1105964d56cdbfe7274e7b779058
seannovela1 <seannovela5484@my.whccd.edu>
2024-11-03 16:18:39 -0800

    added more functions and fixed errors

--- script.js
+++ script.js
@@ -2,12 +2,31 @@
 // Examples:
 // console.log(min(0, 10)); // will output 0.
 // console.log(min(0, -10)); // will output -10
+function min(a,b){
+    return a<b? a:b;
+}
 
 // 2. Write a function that takes the radius of a circle and outputs it’s approximate circumference (the formula for circumference is 2 *pi * radius, you may use 3.14 for pi).
 // Example Output:
 // console.log(calculateCircumference(5)); // will output 31.4
-
+function calculateCircumference(radius)
+{ const pi = 3.14;
+return 2*pi*radius;
+}
 // 3. Write a function searchString that takes a character and then returns how many times it appears in another string argument.
 // Example Output:
 // console.log(searchString(“s”, “mississippi”)); // will output 4
 // console.log(searchString(“q”, “mississippi”)); // will output 0
+function searchstring(char,str){
+    let count = 0;
+    for(let i=0;i<str.length;i++){
+        if(str[i]===char){
+            count++;
+        }
+    
+ 
+    }
+    
+    return count;
+
+}
